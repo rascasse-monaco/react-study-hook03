5a5e241578085053ff0aba44472d8cf9a4320438
rascasse-monaco <mktwkhr@gmail.com>
2021-01-30 21:38:03 +0900

    正規表現の分割代入サンプル

---
+++ sample.js
@@ -0,0 +1,15 @@
+'use strict';
+const parseProtocol = (url) => {
+  const parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url);
+  if (!parsedURL) {
+    return false;
+  }
+  console.log(parsedURL);
+  // ["https://developer.mozilla.org/en-US/docs/Web/JavaScript","https", "developer.mozilla.org", "en-US/docs/Web/JavaScript"]
+
+  const [URL, protocol, fullhost, fullpath] = parsedURL;
+  return protocol;
+}
+
+console.log(parseProtocol('https://developer.mozilla.org/en-US/docs/Web/JavaScript'));
+//"https"
